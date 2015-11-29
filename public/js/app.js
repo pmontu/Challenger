cc4e9aea3ba44aa39a9fc82495a4122fb3eb7913
Suddhasatwa Gupta <suddhasatwa.g@plancess.com>
2015-11-30 03:48:48 +0530

    merge angular app to the backend socket

--- public/js/app.js
+++ public/js/app.js
@@ -1,73 +1,88 @@
 'use strict';
 /**
  * @ngdoc overview
- * @name plancessAssessmentApp
+ * @name collaborativeLearning
  * @description
- * # plancessAssessmentApp
+ * # collaborativeLearning
  *
  * Main module of the application.
  */
- var app = angular
- .module('collaborativeLearning', [
-  'ui.router' 
-  ]);
+angular.module('collaborativeLearning', ['ui.router'])
+.config(['$locationProvider', '$stateProvider', '$urlRouterProvider',
+      function($locationProvider,$stateProvider, $urlRouterProvider) {
+            $locationProvider.html5Mode(false);
 
- app.config(['$locationProvider', '$stateProvider', '$urlRouterProvider',
-  function($locationProvider,$stateProvider, $urlRouterProvider) {
-    $locationProvider.html5Mode(false);
-
-    $urlRouterProvider
-    .otherwise("/home");
+            $urlRouterProvider
+                .otherwise("/home");
 
     $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: 'views/home.html',
-    })
-    .state('create', {
-      url: '/create/',
-      templateUrl: 'views/create.html'
-    })
-    .state('join', {
-      url: '/join/',
-      templateUrl: 'views/join.html',
-      resolve: {
-      }     
-    })
-  }]);
-
-  //var socket = io();
-
-  app.controller("appController", function($scope, socket){
-
-          alert("creating room")
-          socket.emit("create room");
-          socket.on('create room', function(room){
-            console.log(room.owner + " - " + room._id);
-          });
-  })
-
-app.factory('socket', function ($rootScope) {
-  console.log("socket factory")
-  var socket = io("http://192.168.1.107:3000");
-  return {
-    on: function (eventName, callback) {
-      socket.on(eventName, function () {  
-        var args = arguments;
-        $rootScope.$apply(function () {
-          callback.apply(socket, args);
-        });
-      });
-    },
-    emit: function (eventName, data, callback) {
-      socket.emit(eventName, data, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          if (callback) {
-            callback.apply(socket, args);
+      .state('home', {
+        url: '/home',
+        templateUrl: 'views/home.html'
+        })
+      .state('create', {
+        url: '/create/',
+        templateUrl: 'views/create.html',
+        controller:"roomController",
+        resolve: {
+          create:function(socket){         
           }
-        });
+        }     
       })
-    }
-  };
-});
+      .state('join', {
+        url: '/join/',
+        templateUrl: 'views/join.html',
+         controller:"joinController",
+        resolve: {
+        }     
+      })
+      .state('selectquestion', {
+        url: '/selectquestion/:name/',
+        templateUrl: 'views/selectquestion.html',
+         controller:"selectQuestion",
+        resolve: {
+        }     
+      })
+     .state('questionanswer', {
+        url: '/questionanswer/:name',
+        templateUrl: 'views/questionanswer.html',
+        controller: 'questionanswercntrl',
+        resolve: {
+          data:function(){
+            console.log("the appp");
+          }
+        }     
+      })
+     .state('report', {
+        url: '/report',
+        templateUrl: 'views/report.html',
+        controller:'reportcntrl',
+        resolve: {
+          data:function(){
+            console.log("the appp");
+          }
+        }     
+      })
+
+  }]);
+  
+ 
+
+   //var socket = io();
+   /*
+      socket.on('message', function(msg){
+        $('#messages').append($('<li>').text(msg));
+      });
+      socket.on('create room', function(room){
+        $('#messages').append($('<li>').text(room.owner + " - " + room._id));
+      });
+      socket.on('join room', function(playerid){
+        $('#messages').append($('<li>').text(playerid+" joined"));
+      });
+      $("#btnCreateRoom").click(function(){
+        socket.emit("create room");
+      })
+      $("#btnJoinRoom").click(function(){
+        socket.emit("join room", $("#roomid").val());
+      })
+      */
