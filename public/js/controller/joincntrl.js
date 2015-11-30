48e2390a8d802ee78b2f17af36121421d6a02ab9
Manoj Kumar P <manoj.k@plancess.com>
2015-11-30 12:20:09 +0530

    merged. fixed localhost link

--- public/js/controller/joincntrl.js
+++ public/js/controller/joincntrl.js
@@ -7,26 +7,26 @@
  *
  * Main module of the application.
  */
- angular.module('collaborativeLearning')
- .controller('joinController', function ($rootScope,$scope,socket,$http,$state) {
+angular.module('collaborativeLearning')
+  .controller('joinController', function ($rootScope,$scope,socket,$http,$state) {
+  	
+  		$http.get("/rooms").then(function(res){
+  			console.log("rooms length",res.data.length);
+        var len = res.data.length;
+        var id = res.data[len-1]._id;
+  			$scope.roomId = id;
+  			$scope.joingame=function(){
+  				socket.emit("join room",$scope.roomId);
+  			}
 
-  $http.get("/rooms").then(function(res){
-   console.log("rooms length",res.data.length);
-   var len = res.data.length;
-   var id = res.data[len-1]._id;
-   $scope.roomId = id;
-   $scope.joingame=function(){
-    socket.emit("join room",$scope.roomId);
-  }
-
-  socket.on("join room",function(room){
-    $state.go("selectquestion", {name:"Player2"});
+      socket.on("join room",function(room){
+      $state.go("selectquestion", {name:"Player2"});
       //$scope.roomId = room._id;
-    });
-  });
+      });
+  		});
 
 
-
+  	
 
 
 
